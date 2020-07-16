ecc53eacec7822bf05f8acc41465e88ed68617d9
ashaaut <ashaautade1998@gmail.com>
2020-07-16 22:32:10 +0530

    added crud operations

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
 const MongoClient=require('mongodb').MongoClient
+const dboper=require('./operations')
 const assert=require('assert')
 const url="mongodb://localhost:27017";
 const dbname="confusion";
+
 MongoClient.connect(url,(err,client)=>{
     assert.equal(err,null);
     console.log("correctly connected to server");
     const db=client.db(dbname);
-    const collection=db.collection('dishes')
+    // const collection=db.collection('dishes')
 
-    collection.insertOne({"name":"asha"},(err,result)=>{
-        assert.equal(err,null);
-
-        console.log("Arter insert:\n");
-        console.log(result.ops);
-
-        collection.find({}).toArray((err,docs)=>{
-            assert.equal(err,null);
-            console.log("Found:\n")
-            console.log(docs);
-
-            db.dropCollection("dishes",(err,result)=>{
-                assert.equal(err,null);
-                client.close();
-            })
-        })
+    dboper.insertDocument(db,{"name":"vikas","village":"shirasi"},"dishes",(result)=>{
+        console.log("Insert Document: \n",result.ops)
     })
+    dboper.findDocuments(db,"dishes",(docs)=>{
+        console.log("Found Documents:\n",docs)
+    })
+    dboper.updateDocument(db,{"name":"vikas"},{"village":"mangalwedha"},"dishes",(result)=>{
+        console.log("Updated Document:\n", result.result);
+        
+    })
+    dboper.findDocuments(db,"dishes",(docs)=>{
+        console.log(" Updated Found Documents:\n",docs)
+    })
+    db.dropCollection("dishes", (result) => {
+        console.log("Dropped Collection: ", result);
+
+        client.close();
+    });
 })
